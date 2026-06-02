8637fe31b5946829bb0097925ad7b43b46fe9195
SportNest <student@sportnest.com>
2026-06-02 22:24:05 +0600

    auth.js and auth-client update

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,47 +1,34 @@
 import { betterAuth } from "better-auth";
-
-import { mongodbAdapter } from "@better-auth/mongo-adapter"; 
+import { mongodbAdapter } from "@better-auth/mongo-adapter";
 import { MongoClient } from "mongodb";
 import { jwt } from "better-auth/plugins";
 
 let client;
 if (!global._mongoClient) {
-    global._mongoClient = new MongoClient(process.env.MONGODB_URI);
+  global._mongoClient = new MongoClient(process.env.MONGODB_URI);
 }
 client = global._mongoClient;
-
 const db = client.db("sportnest");
 
 export const auth = betterAuth({
-  
-    database: mongodbAdapter(db), 
-    emailAndPassword: {  
-        enabled: true
+  database: mongodbAdapter(db),
+  emailAndPassword: { enabled: true },
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     },
-    socialProviders: {
-        google: { 
-            clientId: process.env.GOOGLE_CLIENT_ID, 
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET, 
-        }, 
+  },
+  trustedOrigins: [
+    "http://localhost:3000",
+    "http://localhost:3001",
+    "https://sport-nest-flax.vercel.app",
+  ],
+  session: {
+    cookieCache: {
+      enabled: true,
+      maxAge: 7 * 24 * 60 * 60,
     },
-
-    
-    trustedOrigins: [
-        "http://localhost:3000",
-        "http://localhost:3001",
-        "https://sport-nest-flax.vercel.app"
-    ],
-    advanced: {
-        disableDefaultEntries: true
-    },
-    session:{
-        cookieCache:{
-            enabled:true,
-            strategy:"jwt",
-            maxAge:7 * 24 * 60 * 60,
-        }
-    },
-    plugins: [
-        jwt(), 
-    ]
+  },
+  plugins: [jwt()],
 });
